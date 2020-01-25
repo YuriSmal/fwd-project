14d9d9ae948530b0ee3207e28953ab5c002b3ebf
Yuri Smal <smalygga1488@gmail.com>
2020-01-25 12:48:49 -0500

    site updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+// анімація якоря
 $(document).ready(function() {
-  $("#anchor").on("click", "a", function(event) {
+  $(".anchor").on("click", "a", function(event) {
     event.preventDefault();
     var id = $(this).attr("href"),
       top = $(id).offset().top;
     $("body,html").animate({ scrollTop: top }, 1500);
   });
 });
+
+//прелоадер
+var $preloader = $("#p_prldr"),
+  $svg_anm = $preloader.find(".svg_anm");
+$svg_anm.fadeOut();
+$preloader.delay(500).fadeOut("slow");
